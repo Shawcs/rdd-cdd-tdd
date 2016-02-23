daee09f57c0bb0125971ea17563de81746525a6d
shawcs <shawcs.t@gmail.com>
2016-02-23 15:22:23 +0100

    format testing

--- weather/test/test-format.js
+++ weather/test/test-format.js
@@ -1,6 +1,11 @@
-var assert = require('assert'),
+var expect = require('chai').expect;
     format = require('./../lib/format');
 
+
+
 describe('format', function () {
-    it('is a constructor of the Format object')
+    it('is a constructor of the Format object',function(){
+expect(format.Format).to.exists;
+    });
+
 });
