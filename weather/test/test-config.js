a2ab665f861858d449afde4295f10ee6ffffac82
shawcs <shawcs.t@gmail.com>
2016-02-23 15:10:35 +0100

    3 test passing with mocha

--- weather/test/test-config.js
+++ weather/test/test-config.js
@@ -1,4 +1,6 @@
-var assert = require('assert'),
+var assert = require('assert');
+var expect = require('chai').expect;
+
     config = require('./../lib/config');
 
 
@@ -9,16 +11,28 @@ var units = {
         speed: 'mph'
     };
 
-var ip = '46.218.45.195'; //added by me
+var ip = '8.8.8.8'; //added by me
 
 
 var Config = new config(units, ip);
 Config.sudo();
 
 describe('Config', function () {
-    it('has 3 props')
 
-    it('after construction, 2 props are populated')
+    it('has 3 props',function(){
+assert.equal(3,Object.keys(Config.units).length)
+    });
 
-    it('has correct key types')
+    it('after construction, 2 props are populated',function(){
+expect(Config.units.tmp).to.exist;
+expect(Config.units.speed).to.exist;
+expect(Config.units.type).to.exist;
+    });
+   
+    it('has correct key types',function(){
+expect(Config.units.tmp).be.a('string');
+expect(Config.units.type).be.a('string');
+expect(Config.units.speed).be.a('string');
+
+    });
 });
